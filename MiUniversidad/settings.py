d63b68dad0962c419aeadd50cc542003424a7487
Johan Gomez <johang0297@gmail.com>
2020-07-18 12:58:20 -0500

    Update settings.py

--- MiUniversidad/settings.py
+++ MiUniversidad/settings.py
@@ -124,5 +124,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "johang0297@gmail.com"
-EMAIL_HOST_PASSWORD = "z7FQl2r8WC"
+EMAIL_HOST_USER = "email@gmail.com"
+EMAIL_HOST_PASSWORD = "password"
